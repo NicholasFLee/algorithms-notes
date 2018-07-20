131931dc50be4d3f7b84c29b54c666dc9ad33306
Nick <2272750086@qq.com>
2018-07-20 17:59:53 +0800

    added C122

--- C122.java
+++ C122.java
@@ -1,9 +1,31 @@
-public interface Comparable {
-    boolean greaterThan(Comparable c);
-    boolean equalTo(Comparable c);
-    boolean lessThan(Comparable c);
+interface Play {
+    void play();
 }
 
-public class C121 {
-    
+class Man implements Play {
+
+    @Override
+    public void play() {
+        System.out.println("man playing");
+    }
+}
+
+class Dog implements Play {
+    @Override
+    public void play() {
+        System.out.println("Dog playing");
+    }
+}
+
+public class C122 {
+    public static void main(String[] args) {
+        Play p1 = new Man();
+        Play p2 = new Dog();
+        play(p1);
+        play(p2);
+    }
+
+    static void play(Play p) {
+        p.play();
+    }
 }
