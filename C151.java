e9bdf9e7db5454569fd523a91efbef72a03a41c9
Nick <2272750086@qq.com>
2018-07-30 20:46:13 +0800

    added path compressed weighted quick union

--- C151.java
+++ C151.java
@@ -85,11 +85,58 @@ class WQU {
         count--;
     }
 }
+
+// Path compressed weighted quick union
+class PCWQU {
+    private int[] id;
+    private int[] sz;
+    private int count;
+
+    public PCWQU(int N) {
+        count = N;
+        id = new int[N];
+        for (int i = 0; i < N; i++) id[i] = i;
+        sz = new int[N];
+        for (int i = 0; i < N; i++) sz[i] = i;
+    }
+
+    public int count() { return count; }
+
+    public boolean connected(int p, int q) { return find(p) == find(q); }
+
+    public int find(int p) {
+        int root = p;
+        int start = p;
+        while (root != id[root]) root = id[root];
+        while (start != root) {
+            int next = id[start];
+            id[start] = root;
+            start = next;
+        }
+        return root;
+    }
+    public void union(int p, int q) {
+        if (connected(p, q)) return;
+        int i = find(p);
+        int j = find(q);
+        if (sz[i] > sz[j]) { id[j] = i; sz[i] += sz[j]; } 
+        else               { id[i] = j; sz[j] += sz[i]; }
+        count--;
+    }
+
+    public void print() {
+        for (int i = 0; i < id.length; i++) {
+            System.out.println("" + i + "->" + id[i]);
+        }
+    }
+}
+
 public class C151 {
     public static void main(String[] args) {
-        qfTest();
-        quTest();
-        wquTest();
+        // qfTest();
+        // quTest();
+        // wquTest();
+        pcwquTest();
     }
 
     public static void qfTest() {
@@ -117,5 +164,16 @@ public class C151 {
         wqu.union(4, 5);
         System.out.println(wqu.connected(0, 3));
         System.out.println(wqu.connected(1, 4));
+    }
+
+    public static void pcwquTest() {
+        PCWQU pcwqu = new PCWQU(10);
+        pcwqu.union(0, 2);
+        pcwqu.union(2, 3);
+        pcwqu.union(3, 4);
+        pcwqu.union(4, 5);
+        System.out.println(pcwqu.connected(0, 3));
+        System.out.println(pcwqu.connected(1, 4));
+        pcwqu.print();
     }
 }
