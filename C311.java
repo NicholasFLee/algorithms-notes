bdd5d02865cfd2dd2347fe103893ac11155ee1a6
Nick <2272750086@qq.com>
2018-08-17 16:48:11 +0800

    adding ST

--- C311.java
+++ C311.java
@@ -1,4 +1,4 @@
-class ST<Key, Value> {
+class ST<Key extends Comparable<Key>, Value> {
     void put(Key k, Value v) {
 
     }
@@ -12,14 +12,54 @@ class ST<Key, Value> {
     }
 
     boolean contains(Key k) {
-        return true;
+        return get(k) != null;
     }
 
     boolean isEmpty() {
-        return true;
+        return size == 0;
     }
 
     int size() { return 1; }
+
+    Key min() {
+        return null;
+    }
+
+    Key max() {
+        return null;
+    }
+
+    Key floor(Key k) {
+        return null;
+    }
+
+    Key ceilling(Key k) {
+        return null;
+    }
+
+    int rank(Key k) {
+        return 0;
+    }
+
+    Key select(int i) {
+        return null;
+    }
+
+    void deleteMin() {
+
+    }
+
+    void deleteMax() {
+
+    }
+
+    int size(Key lo, Key hi) {
+        return 0;
+    }
+
+    Iterable<Key> keys(Key lo, Key hi) {
+        return null;
+    }
 
     Iterable<Key> keys() {
         return null;
