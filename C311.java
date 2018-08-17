e4ae0924475eed04fd8ed0129e44a4a36fb00871
Nick <2272750086@qq.com>
2018-08-17 16:30:40 +0800

    adding ST

---
+++ C311.java
@@ -0,0 +1,32 @@
+class ST<Key, Value> {
+    void put(Key k, Value v) {
+
+    }
+
+    Value get(Key k) {
+        return null;
+    }
+
+    void delete(Key k) {
+
+    }
+
+    boolean contains(Key k) {
+        return true;
+    }
+
+    boolean isEmpty() {
+        return true;
+    }
+
+    int size() { return 1; }
+
+    Iterable<Key> keys() {
+        return null;
+    }
+}
+
+public class C311 {
+
+    
+}
