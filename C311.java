39cf953ecb33b1604b52cf887478136285a6af0e
Nick <2272750086@qq.com>
2018-08-21 19:25:19 +0800

    added sequentialSearchST

--- C311.java
+++ C311.java
@@ -1,65 +1,31 @@
-class ST<Key extends Comparable<Key>, Value> {
-    public void put(Key k, Value v) {
+class SequentialSearchST<Key, Value> {
+    private Node first;
+    private class Node {
+        Key key;
+        Value val;
+        Node next;
 
+        public Node(Key k, Value v, Node n) {
+            this.key = k;
+            this.val = v;
+            this.next = n;
+        }
     }
 
     public Value get(Key k) {
+        for (Node x = first; x != null; x = x.next)
+            if (x.key.equals(k)) return x.val;
         return null;
     }
 
-    public void delete(Key k) {
-
+    public void put (Key k, Value v) {
+        for (Node x = first; x != null; x = x.next)
+            if (x.key.equals(k)) { 
+                x.val = v; 
+                return; 
+            }
+        first = new Node(k, v, first);
     }
-
-    public boolean contains(Key k) {
-        return get(k) != null;
-    }
-
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    public int size() { return 1; }
-
-    public Key min() {
-        return null;
-    }
-
-    public Key max() {
-        return null;
-    }
-
-    public Key floor(Key k) {
-        return null;
-    }
-
-    public Key ceilling(Key k) {
-        return null;
-    }
-
-    public int rank(Key k) {
-        return 0;
-    }
-
-    public Key select(int i) {
-        return null;
-    }
-
-    public void deleteMin() { delete(min()); }
-
-    public void deleteMax() { delete(max()); }
-
-    public int size(Key lo, Key hi) {
-        return 0;
-    }
-
-    public Iterable<Key> keys(Key lo, Key hi) {
-        if (hi.compareTo(lo) < 0) { return 0; }
-        else if (contains(hi)) { return rank(hi) - rank(lo) + 1; }
-        else return rank(hi) - rank(lo);
-    }
-
-    public Iterable<Key> keys() { return keys(min(), max()); }
 }
 
 public class C311 {
