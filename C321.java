0bbafbac1924c084d72a226fb10fa1266f5129e2
Nick <2272750086@qq.com>
2018-08-27 18:36:29 +0800

    adding

---
+++ C321.java
@@ -0,0 +1,39 @@
+class BST<Key extends Comparable<Key>, Value> {
+    private Node root;
+
+    private class Node {
+        private Key key;
+        private Value value;
+        private Node left, right;
+        private int N;
+
+        public Node(Key key, Value value, int N) {
+            this.key = key;
+            this.value = value;
+            this.N = N;
+        }
+    }
+
+    public int size() { return size(root); }
+    private int size(Node n) {
+        if (n == null) return 0;
+        else return n.N;
+    }
+
+    public Value get(Key key) { return get(root, key); }
+    private Value get(Node n, Key key) {
+        if (n == null) { return null; }
+        if (n.key.compareTo(key) == 0) return n.value;
+        else if (n.key.compareTo(key) < 0) return get(n.left, key);
+        else return get(n.right, key);
+    }
+
+    public void put(Key key, Value value) {
+
+    }
+}
+
+public class C321 {
+
+    
+}
