a615ac13aa9d8ea8b05e786704aadb6c1834e4da
Nick <2272750086@qq.com>
2018-08-30 19:23:54 +0800

    added rb node

--- C331.java
+++ C331.java
@@ -1,4 +1,25 @@
 class RedBlackBST<Key extends Comparable<Key>, Value> {
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+    private class Node {
+        Key key;
+        Value val;
+        Node left, right;
+        int N;
+        boolean color; // is red
+
+        Node(Key key, Value val, int N, boolean color) {
+            this.key = key;
+            this.val = val;
+            this.N = N;
+            this.color = color;
+        }
+    }
+
+    private boolean isRed(Node n) {
+        if (n == null) return false;
+        return n.color == RED;
+    }
 }
 
 public class C331 {
